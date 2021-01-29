4b5272028b6e89a0b9d6532062c6b0ed8b4b4783
gabi <aragao.gabi@gmail.com>
2021-01-29 11:34:28 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace Csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            string[] a = new string[3];
+            a[0] = "Gabriela";
+            a[1] = "Gerson";
+            a[2] = "Desiree";
+            foreach(string nome in a){
+                Console.WriteLine(nome);
+
+            }      
+        }        
+    }
+}
